931b803aac3e297228e1d0f1e52a9c602d733ded
ELehotzky <Lehotzky.Erica@gmail.com>
2018-09-10 17:29:27 -0500

    first commit

---
+++ db/migrate/20180910220412_create_drinks.rb
@@ -0,0 +1,11 @@
+class CreateDrinks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :drinks do |t|
+      t.string :name
+      t.integer :likes
+      t.belongs_to :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
