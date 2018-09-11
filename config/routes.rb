b525d0f3964a1769ea257907732dcb68bdf0ca32
Kelly <devkells00@gmail.com>
2018-09-11 15:35:43 -0500

    Updated routes again

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
 				resources :users
 				resources :drinks
 				resources :ingredients
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+		end
+	end
 end
